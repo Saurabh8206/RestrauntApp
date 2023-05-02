0734f5f224c30ddcbab8df6a5d5ad51228bdba6b
Saurabh kohade <66825179+Saurabh8206@users.noreply.github.com>
2023-05-02 12:11:05 +0530

    Initial Project structure

---
+++ restraunt/src/test/java/com/app/restraunt/RestrauntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.restraunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestrauntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
